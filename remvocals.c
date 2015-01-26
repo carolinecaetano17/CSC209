6451231a11bce146138021a9b1cfec77385fa5ff
Carol <carol@debian>
2015-01-25 20:01:41 -0500

    remvocals 3# version

--- remvocals.c
+++ remvocals.c
@@ -26,18 +26,22 @@ int main(int argc, char **argv){
 
   	void *buffer = malloc(44);
 
-  	fread (buffer,1,44,from);
-  	fwrite ( buffer, 1, 44, to );
+  	fread (buffer,44,1,from);
+  	printf("%s\n",(char*)buffer );
+  	fwrite ( buffer, 44, 1, to );
   	printf("Wrote %d\n",sizeof(short));
+
   	while (fgetc(from) != EOF) {
   		//printf("W1\n");
   		fread (values,sizeof(short),2,from);
-  		newValue = 0;
-  		newValue += values[0];
-  		newValue += values[1];
-  		newValue = newValue/2;
+
+  		newValue = (values[0] - values[1])/2;
+  	
+  		printf("BEFORE left: %d right: %d new: %d\n",values[0],values[1],newValue );
   		values[0] = values[1] = newValue;
+  		printf("AFTER left: %d right: %d new: %d\n",values[0],values[1],newValue );
   		fwrite ( values,sizeof(short),2, to );
+  		
   		
   	}
 
@@ -45,4 +49,5 @@ int main(int argc, char **argv){
   	fclose(to);
   	free(buffer);
   	free(values);
+  	return 0;
 }
